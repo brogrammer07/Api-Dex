5e884183a220c019ad0a3c830845451ad9f4d493
Tushar Tomar <tomartushar5@gmail.com>
2021-12-31 22:43:46 +0530

    Change in packages

--- components/ParticleBackground.js
+++ components/ParticleBackground.js
@@ -1,8 +1,6 @@
-import Particles from "react-particles-js";
+import Particles from "react-tsparticles";
 import particlesConfig from "../config/particle-config";
 
-export default function ParticlesBackground(){
-    return(
-        <Particles canvasClassName="absolute" params={particlesConfig} />
-    );
+export default function ParticlesBackground() {
+  return <Particles canvasClassName="absolute" params={particlesConfig} />;
 }
